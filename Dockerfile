45ff2be998ee81842ce405a69ef36ffc19231ead
Adam McMaster <adam@zooniverse.org>
2017-02-24 15:35:29 +0000

    Add Let's Encrypt certbot

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,18 @@
 FROM ubuntu:14.04
 
 RUN apt-get update && \
-    apt-get -y -q install nginx logrotate supervisor openssl && \
+    apt-get -y -q install \
+        nginx \
+        logrotate \
+        supervisor \
+        openssl \
+        curl \
+        && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN curl -o /usr/local/bin/certbot-auto https://dl.eff.org/certbot-auto && \
+    chmod +x /usr/local/bin/certbot-auto && \
+    yes y | /usr/local/bin/certbot-auto --os-packages-only && \
     rm -rf /var/lib/apt/lists/*
 
 ADD conf/logrotate.conf /etc/logrotate.d/nginx
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,31 @@
 #/bin/bash -e
 
 mkdir -p /etc/nginx/ssl/
-openssl req \
-    -new \
-    -newkey rsa:4096 \
-    -days 365 \
-    -nodes \
-    -x509 \
-    -subj "/C=NA/ST=NA/L=NA/O=NA/CN=$(hostname)" \
-    -keyout /etc/nginx/ssl/key.pem \
-    -out /etc/nginx/ssl/cert.pem
+
+if [ -z "$CERTBOT_VHOST" ]
+then
+    openssl req \
+        -new \
+        -newkey rsa:4096 \
+        -days 365 \
+        -nodes \
+        -x509 \
+        -subj "/C=NA/ST=NA/L=NA/O=NA/CN=$(hostname)" \
+        -keyout /etc/nginx/ssl/key.pem \
+        -out /etc/nginx/ssl/cert.pem
+else
+    if [ ! -f /etc/nginx/ssl/cert.pem ]
+    then
+        yes y | /usr/local/bin/certbot-auto certonly \
+            --standalone \
+            --no-self-upgrade \
+            --agree-tos \
+            --email sysadmins@zooniverse.org \
+            -n \
+            -d ${CERTBOT_VHOST}
+        ln -s /etc/letsencrypt/live/${$CERTBOT_VHOST}/privkey.pem /etc/nginx/ssl/key.pem
+        ln -s /etc/letsencrypt/live/${$CERTBOT_VHOST}/cert.pem /etc/nginx/ssl/cert.pem
+    fi
+fi
 
 exec /usr/bin/supervisord
